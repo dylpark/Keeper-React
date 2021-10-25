94255cadd0abab68fbf43e53af88289eb2b44db8
Dylan Park <dylpark@protonmail.com>
2021-10-26 09:48:25 +1000

    Update Note.jsx

--- keeper/src/components/Note.jsx
+++ keeper/src/components/Note.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Note(props) {
+  const handleClick = () => {
+    props.onDelete(props.id);
+  };
+
   return (
     <div className="note">
-      <h1 contentEditable="true" spellCheck="true">
-        {props.title}
-      </h1>
-      <p contentEditable="true" spellCheck="true">
-        {props.content}
-      </p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 }
