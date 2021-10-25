d54e0dfd318947e4252cc28fc760651e2b40bff8
Dylan Park <dylpark@protonmail.com>
2021-10-26 09:48:22 +1000

    Update App.jsx

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -13,11 +13,29 @@ function App() {
     });
   };
 
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      <Note key={1} title="Note title" content="Note content" />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
