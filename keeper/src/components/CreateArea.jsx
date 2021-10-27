a1e60dc62a05ef0d63087f35eb7bcd8a53e806ca
Dylan Park <dylpark@protonmail.com>
2021-10-27 17:53:58 +1000

    Adding Dependencies & Styling

--- keeper/src/components/CreateArea.jsx
+++ keeper/src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: "",
@@ -26,23 +31,35 @@ function CreateArea(props) {
     event.preventDefault();
   };
 
+  const expand = () => {
+    setExpanded(true);
+  };
+
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
         <textarea
+          onClick={expand}
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- keeper/src/components/Header.jsx
+++ keeper/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import CreateIcon from "@mui/icons-material/Create";
 
 function Header() {
   const header = "Keeper";
 
   return (
     <header>
-      <h1>{header}</h1>
+      <h1>
+        <CreateIcon color="white" />
+        {header}
+      </h1>
     </header>
   );
 }
